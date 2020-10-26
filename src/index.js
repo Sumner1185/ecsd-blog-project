d3e3130d42336feafd9a05ef5bf1901c6599d985
Alastair Sumner <alastair-sumner@ecs-digital.co.uk>
2020-10-26 17:22:38 +0000

    Add server details

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.set('port', process.env.PORT || 4000)
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use('/', require('./routes/blog-routes'))
+
+app.listen(app.get('port'), () => console.log(`Server on port ${app.get('port')}`))
