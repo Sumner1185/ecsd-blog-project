2ff091e469b8e4545237d044844e33d7f84ff22d
Alastair Sumner <alastair-sumner@ecs-digital.co.uk>
2020-10-27 09:29:21 +0000

    Remove mocha and chai required in test file

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,8 @@
-const { expect } = require("chai")
-const { test } = require("mocha")
-
 Routes = require('../src/routes/blog-routes')
     
     describe('Mock test', () => {
     it('Should pass', () => {
         const testPhrase = "Passing test"
-        expect(testPhrase).to.equal("Passing test")
+        expect(testPhrase).toEqual("Passing test")
     })
 })
