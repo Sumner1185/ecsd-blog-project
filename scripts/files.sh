c25ab83a34fe449e440912eadfaa57a996764d03
JoeJacques <joe.lewis.jacques@gmail.com>
2020-10-27 12:00:00 +0000

    feat: shell script to download and validate files

---
+++ scripts/files.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd ../src/blogs
+
+curl https://academy-project-blogs.s3-eu-west-1.amazonaws.com/teaching_code.doc > teaching_code.doc 
+curl https://academy-project-blogs.s3-eu-west-1.amazonaws.com/IDC.md > IDC.md
+curl https://academy-project-blogs.s3-eu-west-1.amazonaws.com/milestones.txt > milestones.txt
+
+fileNames=$(ls)
+
+
+for file in ${fileNames} ; do 
+    echo filename: "${file%%.*}" 
+    echo file extention: "${file#*.}" 
+    echo file size: && wc -c ${file} | awk '{print $1}'
+done;
+
+
+
+
