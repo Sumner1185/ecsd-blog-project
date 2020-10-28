17f66af253c32b1013be0ffac0ebca1e08052719
Alastair Sumner <alastair-sumner@ecs-digital.co.uk>
2020-10-28 14:26:42 +0000

    test: add post route test

--- test/mockIndex.test.js
+++ test/mockIndex.test.js
@@ -1,26 +1,39 @@
 const app = require('../index')
 const supertest = require('supertest')
 const request = supertest(app)
-    
-describe('Mock test', () => {
-    
-    test('should pass', () => {
-        const testPhrase = "Passing test"
-        expect(testPhrase).toEqual("Passing tester")
-    })
 
-})
-
-describe('mock test api endpoint', () => {
+describe('mock test api endpoints', () => {
 
     app.get('/test', async (req, res) => {
         res.json({ message: 'pass!' })
     })
 
-    it('gets the test endpoint', async done => {
+    app.post('/test', async (req, res) => {
+        let blogs = []
+        const testBlog = { 
+            "id": (blogs.length + 1).toString(),
+            "content": "Test blog"
+        }
+        if (testBlog.id && testBlog.content) {
+            blogs.push(testBlog)
+            res.json(testBlog)
+        } else {
+            res.status(400).json({ error: 'Missing information' })
+        }
+    })
+
+    it('GETs the test endpoint', async done => {
         const res = await request.get('/test')
         expect(res.status).toBe(200)
         expect(res.body.message).toBe('pass!')
+        done()
+    })
+
+    it('Adds an entry to the blog list', async done => {
+        const res = await request.post('/test')
+        console.log(res.body[0])
+        expect(res.body.content).toEqual('Test blog')
+        expect(res.body.id).toEqual("1")
         done()
     })
 })
