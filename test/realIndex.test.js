691d2f7cd6366bf6517a63a97e7ff42cebf27828
Alastair Sumner <alastair-sumner@ecs-digital.co.uk>
2020-10-28 18:53:46 +0000

    test: add test for post error

--- test/realIndex.test.js
+++ test/realIndex.test.js
@@ -1,41 +1,5 @@
-// const express = require("express"); 
-// const Routes = require("../src/routes/blog-routes"); 
-// const request = require("supertest"); 
-// const app = express(); 
-// // const axios = require('axios');
-// app.use("/blogs", Routes);
-// jest.mock('axios');
-
 const request = require('supertest')
 const app = require('../index')
-
-
-//  describe('live api endpoints', () => {  
-
-    // test('Get', async done => {
-    //     axios.get('/blogs')
-    //     done()
-    // })
-
-    // test('POST', async done => {
-    //     axios.post('/', {
-    //         "id": 4,
-    //         "content": "test"
-    //     })
-    //     done()
-    // })
-
-//     test('GET route', async done => {
-//         const { body } = await request(app).get('/blogs')
-//         expect(body.length).toEqual(3)
-//         expect(body[0].id).toEqual(1)
-//         done()
-//     })
-
-//     test('POST route', async done => {
-//         app.post('/')
-//     })
-//  })
 
  describe('API emdpoints', () => {
 
@@ -58,5 +22,15 @@ const app = require('../index')
         expect(res.statusCode).toEqual(200)
         expect(res.body).toHaveProperty('content')
         expect(res.body).toHaveProperty('title')
+    })
+
+    it('should error with 400 if post data incorrect', async () => {
+        const res = await request(app)
+        .post('/blogs')
+        .send({
+            "id": 1,
+            "header": "test"
+        })
+        expect(res.statusCode).toEqual(400)
     })
 })
